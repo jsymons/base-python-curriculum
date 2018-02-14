11f46a6a4a65c7c59732c80ce7a015bc40258c3d
jasonmeeks <jasonmeeks42@gmail.com>
2018-02-14 17:56:41 -0600

    py3 strikes again

--- unit-18-quiz-week-2/lesson-5-censor-your-dictionary/solutions/solution_.py
+++ unit-18-quiz-week-2/lesson-5-censor-your-dictionary/solutions/solution_.py
@@ -1,5 +1,8 @@
 def censor_dictionary(unclean_dictionary, flagged_word):
+    removal_list = []
     for word, description in unclean_dictionary.items():
         if flagged_word in description:
-            unclean_dictionary.pop(word, None)
+            removal_list.append(word)
+    for word in removal_list:
+        unclean_dictionary.pop(word, None)
     return unclean_dictionary
