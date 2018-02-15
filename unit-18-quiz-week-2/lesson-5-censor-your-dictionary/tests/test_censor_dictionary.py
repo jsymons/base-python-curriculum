37aa335bec7645d49da334aa2c0ccd3f18af0541
Santiago Basulto <santiago.basulto@gmail.com>
2018-02-14 23:03:22 -0300

    Final tuning

--- unit-18-quiz-week-2/lesson-5-censor-your-dictionary/tests/test_censor_dictionary.py
+++ unit-18-quiz-week-2/lesson-5-censor-your-dictionary/tests/test_censor_dictionary.py
@@ -1,8 +1,10 @@
 def test_censor_dictionary():
     expressions = {
-                   "pumped": "I'm so darn excited!",
-                   "happy": "Yeehaw",
-                   "agreeable": "darn tootin!"
+       "pumped": "I'm so darn excited!",
+       "happy": "Yeehaw",
+       "agreeable": "darn tootin!"
     }
 
-    assert censor_dictionary(expressions, "darn") == {"happy": "Yeehaw"}
+    assert censor_dictionary(expressions, "darn") == {
+        "happy": "Yeehaw"
+    }
