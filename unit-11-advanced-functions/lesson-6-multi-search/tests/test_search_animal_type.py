ffcceea19d9806eb67a4da926e471e5028adaa61
Jason Meeks <jason@rmotr.com>
2018-02-20 14:17:53 -0600

    last default args assignment

--- unit-11-advanced-functions/lesson-6-multi-search/tests/test_search_animal_type.py
+++ unit-11-advanced-functions/lesson-6-multi-search/tests/test_search_animal_type.py
@@ -1,17 +1,4 @@
-expected = [
-    {
-        "animal_type": "Penguin",
-        "street_name": "Formal Chicken",
-        "given_name": "Mclovin"
-    },
-    {
-        "animal_type": "Seagull",
-        "street_name": "Beach Chicken",
-        "given_name": "Santiago"
-    }
-]
-
-expected = [{
+expected_animal_types = [{
     'animal_type': 'Penguin',
     'given_name': 'Mclovin',
     'street_name': 'Formal Chicken'
@@ -22,5 +9,4 @@ expected = [{
 ]
 
 def test_search_animal_type():
-    #assert False, (search_animals(animals, animal_type="Chicken"))
-    assert search_animals(animals, animal_type="Chicken") == expected
+    assert search_animals(animals, animal_type="Chicken") == expected_animal_types
--- unit-11-advanced-functions/lesson-6-multi-search/tests/test_search_given_name.py
+++ unit-11-advanced-functions/lesson-6-multi-search/tests/test_search_given_name.py
@@ -1,8 +1,8 @@
-expected = [{
+expected_given = [{
     "animal_type": "Ferret",
     "street_name": "Cat Snake",
     "given_name": "Will Ferret"
 }]
 
 def test_search_given_name():
-    assert search_animals(animals, given_name="Will Ferret") == expected
+    assert search_animals(animals, given_name="Will Ferret") == expected_given
