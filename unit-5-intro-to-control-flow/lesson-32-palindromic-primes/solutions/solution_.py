47818c915b1517e638ffe9a77711ec24880a8eff
Santiago Basulto <santiago.basulto@gmail.com>
2018-05-05 17:19:37 -0300

    Update solution_.py

--- unit-5-intro-to-control-flow/lesson-32-palindromic-primes/solutions/solution_.py
+++ unit-5-intro-to-control-flow/lesson-32-palindromic-primes/solutions/solution_.py
@@ -9,9 +9,18 @@ def is_prime(n):
         return True
     if n % 2 == 0:
         return False
-    for i in range(3, n-1, 2):
+
+    i = 3
+    while i < n-1:
         if n % i == 0:
             return False
+        i += 2
+
+    # Could have been written with a for-loop
+    # for simplicity:
+    #for i in range(3, n-1, 2):
+    #    if n % i == 0:
+    #        return False
     return True
 
 def next_palindromic_prime(n):
