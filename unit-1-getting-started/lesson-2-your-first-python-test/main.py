20bd571fc36ce89f6999f778be100f2a417b47ff
Santiago Basulto <santiago.basulto@gmail.com>
2018-07-06 14:43:42 -0300

    New first python test

---
+++ unit-1-getting-started/lesson-2-your-first-python-test/main.py
@@ -0,0 +1,5 @@
+your_name = ""
+course = "RMOTR Python"
+
+greeting_message = "Welcome {} to the {} course!".format(your_name, course)
+print(greeting_message)
---
+++ unit-1-getting-started/lesson-2-your-first-python-test/tests/test_name_is_not_blank.py
@@ -0,0 +1,2 @@
+def test_name_is_not_blank():
+    assert your_name != ''
