20dd792c8984f01d8b52dbc1b699f01aa544c727
jsymons <jason.symons@gmail.com>
2019-02-24 21:38:17 -0500

    Correction to solution #2
    In reference to https://github.com/rmotr-curriculum/base-python-curriculum-questions/issues/353

--- unit-18-quiz-week-2/lesson-3-sum-of-dict-values/solutions/solution_multiple_functions.py
+++ unit-18-quiz-week-2/lesson-3-sum-of-dict-values/solutions/solution_multiple_functions.py
@@ -2,7 +2,7 @@ def _add_values_to_dict(processing_dict, agg_dict):
     for key, value in processing_dict.items():
         agg_dict.setdefault(key, 0)
         new_value = None
-        if type(value) is int:
+        if type(value) is int and agg_dict[key] is not None:
             new_value = agg_dict[key] + value
         agg_dict[key] = new_value
 
